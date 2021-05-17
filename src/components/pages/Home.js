5c1b8ea8f3cd3c09ebb876f79f5f3e099c0af8d9
Kelvin Kellner <kelvinkellner123@gmail.com>
2021-05-17 11:17:03 -0400

    Added smiley easter egg
    Try moving deployment to a different branch, so main branch can be source code

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -11,7 +11,23 @@ import Heading from '../Heading';
 import MeImg from '../../assets/images/kelvin-prom.jpg';
 import SocialButtons from '../SocialButtons';
 
+let count = 0;
+
 class Home extends Component {
+    smileyEasterEgg() {
+        count++;
+        const smiley = document.getElementById('smiley');
+        const clone = smiley.cloneNode(true);
+        clone.style.position = 'absolute';
+        clone.style.top = document.getElementById('title').getBoundingClientRect().top;
+        clone.id = 'smiley-clone-' + count;
+        smiley.before(clone);
+        clone.classList.add('smiley-easter-egg');
+        window.setTimeout(function () {
+            clone.remove();
+        }, 5000);
+    }
+
     render() {
         return (
             <Container id="home-page" className="main">
@@ -22,7 +38,7 @@ class Home extends Component {
                     <Col sm={12} lg={8} xl={8} style={{ padding: 0, margin: "3.2em 0 0 0.2em" }}>
                         <Heading>Passionate About Learning <span>&amp;</span> Creating<span>.</span></Heading>
                         <p>
-                            Hello! <svg id="smiley" style={{ visibility: "hidden", margin: "-16px" }} xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                            <Button variant="link" className="btn-secret-link" onClick={this.smileyEasterEgg}>Hello!</Button><svg id="smiley" style={{ visibility: "hidden", margin: "-18px" }} xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24"> 
                             <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 14h-12c.331 1.465 2.827 4 6.001 4 3.134 0 5.666-2.521 5.999-4zm0-3.998l-.755.506s-.503-.948-1.746-.948c-1.207 0-1.745.948-1.745.948l-.754-.506c.281-.748 1.205-2.002 2.499-2.002 1.295 0 2.218 1.254 2.501 2.002zm-7 0l-.755.506s-.503-.948-1.746-.948c-1.207 0-1.745.948-1.745.948l-.754-.506c.281-.748 1.205-2.002 2.499-2.002 1.295 0 2.218 1.254 2.501 2.002z"/></svg>
                             I am a software developer &amp; visual designer currently studying Computer Science, User-Experience Design, &amp; Mathematics at Wilfrid Laurier University. 
                             I have an affection for building new skills and helping others grow.</p>
