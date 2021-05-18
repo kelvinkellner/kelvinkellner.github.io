7e9875efc6397dd9cdc097e4cf040b820fd9a516
Kelvin Kellner <kelvinkellner123@gmail.com>
2021-05-18 18:06:35 -0400

    Fixed minor typo

--- src/components/pages/Dev.js
+++ src/components/pages/Dev.js
@@ -31,7 +31,7 @@ class Dev extends Component {
                     <Heading>Software <span>&amp;</span> Website Development<span>.</span></Heading>
                 </Row>
                 <Row>
-                    <p>Puzzles have always fascinated me. From when I was a kid playing with Rubik's Cube, playing challenging board games, or learning to code – I have never been the type of person to shy away from challenges. I am a problem-solver after all. I love working with others to analyse issues, brainstorm ideas, and begin implementing solutions.</p>
+                    <p>Puzzles have always fascinated me. From when I was a kid playing with Rubik's Cubes, playing challenging board games, or learning to code – I have never been the type of person to shy away from challenges. I am a problem-solver after all. I love working with others to analyse issues, brainstorm ideas, and begin implementing solutions.</p>
                     <p>University has given me the opportunity to learn tons of new information about mathematics, computing, and algorithms. I have also gained a strong interest in UI/UX Design and creating products that are easy to use and serve some benefit to users. I hope to continue exploring the worlds of software, website, and mobile development in the years to come. Until then, you can find some of the past projects I have worked on below.</p>
                 </Row>
                 <Row style={{marginTop: "2em"}}>
